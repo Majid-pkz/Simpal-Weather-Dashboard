3514fc0ee2521767d6ac7fbce614e6e16c906adb
Majid-pkz <majid.computing@gmail.com>
2023-01-15 01:35:36 +1100

    add javascript

---
+++ script.js
@@ -0,0 +1,189 @@
+
+var userGeneratedUrl=""
+var geoUrl=""
+var lat=0;
+var lon=0;
+var country="";
+var cityName;
+
+const apiKey="8c2432d7de8348b1ad8d9b6d6fb89aea";
+
+// local time displayed at the header 
+var localTimeEl= $("#local-time");
+ function displayLocalTime(){
+    var localTimeValue=dayjs().format('MMM DD, YYYY [at] HH');
+    localTimeEl.text(localTimeValue); }
+ displayLocalTime();
+ setInterval(displayTime, 1000); 
+ var currentTimeEl= document.getElementById("current-time");
+function displayTime() {
+    var rightNow = dayjs().format('MMM DD, YYYY ');    
+    currentTimeEl.textContent="( "+ rightNow+" )" 
+  }
+  displayTime();
+  setInterval(displayTime, 1000); 
+
+// get elements by Id-----------------------------------------------------------------
+  var todayTempEl=document.getElementById('today-temp')
+  var todayWindEl=document.getElementById('today-wind')
+  var todayHumidityEl= document.getElementById('today-humidity')
+
+  var day1TempEl=document.getElementById('day1-temp')
+  var day1WindEl=document.getElementById('day1-wind')
+  var day1HumidityEl=document.getElementById('day1-humidity')
+
+  var day2TempEl=document.getElementById('day2-temp')
+  var day2WindEl= document.getElementById('day2-wind')
+  var day2HumidityEl=document.getElementById('day2-humidity')
+
+  var day3TempEl=document.getElementById('day3-temp')
+  var day3WindEl= document.getElementById('day3-wind')
+  var day3HumidityEl=document.getElementById('day3-humidity')
+
+  var day4TempEl=document.getElementById('day4-temp')
+  var day4WindEl= document.getElementById('day4-wind')
+  var day4HumidityEl=document.getElementById('day4-humidity')
+
+  var day5TempEl=document.getElementById('day5-temp')
+  var day5WindEl= document.getElementById('day5-wind')
+  var day5HumidityEl=document.getElementById('day5-humidity')
+
+
+  //----------------------------------------------------------------------------------------
+  
+var inputEl=document.getElementById("user-input");
+var cityDisplayEl= document.getElementById("main-city");
+
+
+function captureCityName(event){
+    event.preventDefault();
+ 
+   var cityTyped=inputEl.value
+   userGeneratedUrl="http://api.openweathermap.org/geo/1.0/direct?q="+cityTyped+"&limit=2&appid=8c2432d7de8348b1ad8d9b6d6fb89aea"
+
+   fetch(userGeneratedUrl).then(data=>{
+    console.log(data)
+    return data.json()
+     
+    })
+    .then(resp1=>{      
+      lat=resp1[0].lat
+      lon=resp1[0].lon
+      country=resp1[0].country
+      cityName=resp1[0].name                
+      cityDisplayEl.textContent=cityName;
+      // create a api based on city entered by user
+      geoUrl="https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=8c2432d7de8348b1ad8d9b6d6fb89aea"+"&units=metric"
+      console.log(geoUrl) 
+
+      fetch(geoUrl).then(dataaa=>{
+        console.log(dataaa)
+        return dataaa.json()
+         
+        })
+        .then(respons=>{
+                console.log(respons)
+             
+              return respons.list
+              }) 
+              .then(info=>{
+                // to filter the tepm at different time of the day 6:00 am  12 pm and 18:00 
+                console.log(info)
+                var timeSlot=12;
+                if(dayjs().format(' HH')<9){
+                  timeSlot=6;
+                  console.log(timeSlot)
+                }
+                if(dayjs().format(' HH')>=9&& dayjs().format(' HH')<18){
+                  timeSlot=12;
+                  console.log("timeslot :"+timeSlot+':00:00')
+                }
+                if( dayjs().format(' HH')>=18){
+                  timeSlot=18;
+                  console.log(timeSlot)
+                }              
+
+
+                var timeInt=parseInt(dayjs().format(' HH'))
+                var timeText=dayjs().format(' HH')
+                var minText=":00:00"
+                console.log(typeof(timeInt))
+                console.log(typeof( timeInt.toString()))
+                console.log((timeSlot+':00:00'))
+
+                  for(var i=0;i<info.length;i++){
+
+                  if(info[i].dt_txt.includes((timeSlot+':00:00')))
+                  {
+                   todayTemp=info[i].main.temp;
+                   todayWind=info[i].wind.speed;
+                   todayHumidity=info[i].main.humidity                  
+                  
+                   todayTempEl.textContent=todayTemp;
+                   todayWindEl.textContent=todayWind;
+                   todayHumidityEl.textContent=todayHumidity;
+                  }
+                  var day1= dayjs().add(1,'day').format('YYYY-MM-DD');
+                  if(info[i].dt_txt.includes(day1)&& info[i].dt_txt.includes((timeSlot+':00:00'))){
+                    day1Temp=info[i].main.temp;
+                    day1Wind=info[i].wind.speed;
+                    day1Humidity=info[i].main.humidity ;
+                    console.log(day1Temp);
+                    console.log(day1Wind);
+                    console.log(day1Humidity);
+
+                    day1TempEl.textContent=day1Temp;
+                    day1WindEl.textContent=day1Wind;
+                    day1HumidityEl.textContent=day1Humidity;
+                  }
+                  var day2= dayjs().add(2,'day').format('YYYY-MM-DD');
+                  if(info[i].dt_txt.includes(day2)&& info[i].dt_txt.includes((timeSlot+':00:00'))){
+                    day2Temp=info[i].main.temp;
+                    day2Wind=info[i].wind.speed;
+                    day2Humidity=info[i].main.humidity ;  
+
+                    day2TempEl.textContent=day2Temp;
+                    day2WindEl.textContent=day2Wind;
+                    day2HumidityEl.textContent=day2Humidity;
+                  }
+                  var day3= dayjs().add(3,'day').format('YYYY-MM-DD');
+                  if(info[i].dt_txt.includes(day3)&& info[i].dt_txt.includes((timeSlot+':00:00'))){
+                    day3Temp=info[i].main.temp;
+                    day3Wind=info[i].wind.speed;
+                    day3Humidity=info[i].main.humidity ;
+                    
+                    day3TempEl.textContent=day3Temp;
+                    day3WindEl.textContent=day3Wind;
+                    day3HumidityEl.textContent=day3Humidity;
+                  }
+                  var day4= dayjs().add(4,'day').format('YYYY-MM-DD');
+                  if(info[i].dt_txt.includes(day4)&& info[i].dt_txt.includes((timeSlot+':00:00'))){
+                    day4Temp=info[i].main.temp;
+                    day4Wind=info[i].wind.speed;
+                    day4Humidity=info[i].main.humidity ;
+                    
+                    day4TempEl.textContent=day4Temp;
+                    day4WindEl.textContent=day4Wind;
+                    day4HumidityEl.textContent=day4Humidity;
+                  }
+                  var day5= dayjs().add(5,'day').format('YYYY-MM-DD');
+                  if(info[i].dt_txt.includes(day5)&& info[i].dt_txt.includes((timeSlot+':00:00'))){
+                    day5Temp=info[i].main.temp;
+                    day5Wind=info[i].wind.speed;
+                    day5Humidity=info[i].main.humidity ;
+                    
+                    day5TempEl.textContent=day5Temp;
+                    day5WindEl.textContent=day5Wind;
+                    day5HumidityEl.textContent=day5Humidity;
+                  }
+                }
+              }) 
+             
+
+           })        
+ 
+}
+
+// Event listnener for search button
+var searchButtonEL=document.getElementById("search-btn")
+searchButtonEL.addEventListener("click",captureCityName)
